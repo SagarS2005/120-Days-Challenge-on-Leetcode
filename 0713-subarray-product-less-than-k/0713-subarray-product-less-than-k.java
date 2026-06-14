917e287409044b3d96ea130026ee3569d8fe7ea1
Sagar Sahu <sagarsahuji000@gmail.com>
2026-06-14 13:49:52 +0530

    Time: 4 ms (99.82%), Space: 51.3 MB (58.77%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -1,20 +1,18 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        int count =0;
-        if(k <=1){
-            return 0;
-        }
-        for(int i=0; i<nums.length; i++){
-            int product=1;
-            
-            for(int j=i; j<nums.length; j++){
-                product *= nums[j];
+        if ( k <= 1) return 0;
+        int l=0; 
+        int product = 1;
+        int count = 0;
 
-                if(product <k) count++;
-                else{
-                    break;
-                }
+        for(int r = 0 ; r<nums.length; r++){    // expansion phase of window like ([2], [2,3],[2,3,4])
+            product *= nums[r];
+
+            while (product >= k) {        // Shrinking phase of window eg: ( [2,3,4] -> [3,4] , here 2 removed)
+                product /= nums[l];
+                l++;
             }
+            count += r-l+1;
         }
         return count;
     }
