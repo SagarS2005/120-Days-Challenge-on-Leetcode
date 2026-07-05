d9af3d3770d5fdd6a2dd373c930458d3db1f0e5a
Sagar Sahu <sagarsahuji000@gmail.com>
2026-07-05 22:33:38 +0530

    Time: 7 ms (68.72%), Space: 46.8 MB (22.07%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.java
+++ 1314-matrix-block-sum/1314-matrix-block-sum.java
@@ -2,22 +2,46 @@ class Solution {
     public int[][] matrixBlockSum(int[][] mat, int k) {
         int rows = mat.length;
         int cols = mat[0].length;
-        int answer [][] = new int[rows][cols];
-        for(int i = 0 ; i<rows; i++){
-            for(int j=0; j<cols; j++){
-                int sum = 0;
 
-                for(int r=i-k; r<=i+k; r++){
-                    for(int c=j-k; c <= j+k; c++){
-                        
-                        if( r >= 0 && r < rows && c >= 0 && c < cols){
-                            sum += mat[r][c];
-                        }
-                    }
-                }
-                answer[i][j] = sum;
+        // For Prefix Matrix
+        int [][] prefix = new int [rows + 1 ][cols + 1];
+
+        for (int i = 1 ; i <= rows ; i++) {
+            for ( int j = 1 ; j <= cols ; j++ ) {
+                prefix[i][j] = mat [i - 1][ j - 1] 
+                               + prefix [ i - 1][ j ] 
+                               + prefix [ i ][j - 1] 
+                               - prefix [ i - 1 ][ j - 1];
             }
         }
-        return answer;
+
+        // For Ans Matrix 
+        int [][] ans = new int [rows][cols];
+        
+        for ( int i = 0 ; i < rows; i++ ) {
+            for ( int j = 0; j < cols ; j++) {
+                int r1 = Math.max(0, i - k );    // it ensure that minimum row will be 0
+                int c1 = Math.max(0, j - k );
+
+                // it ensure that max rows and colums should not go outside of matrix 
+                int r2 = Math.min(i + k, rows - 1);
+                int c2 = Math.min (j + k, cols - 1);
+
+                // Since we created Prefix Matrix with "rows + 1" and "cols + 1" from original matrix means if original matrix have 3 rows and 3 cols and prefix matrix will have 4 rows and 4 cols and starting first row and first column will be 0 because we started inserting values from i = 1 and j = 1 
+
+                r1++;
+                r2++;
+                c1++;
+                c2++;
+
+                
+
+                ans[ i ] [ j ] = prefix [ r2 ][ c2 ]
+                                 - prefix[ r1 - 1][ c2 ] 
+                                 - prefix[ r2 ][ c1 - 1 ] 
+                                 + prefix [ r1 - 1] [ c1 - 1];
+            }
+        }
+        return ans;
     }
 }
