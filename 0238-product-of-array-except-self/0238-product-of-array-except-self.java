19a27a25f08fd6051f59ec48e3dc6e7ac74053b9
Sagar Sahu <sagarsahuji000@gmail.com>
2026-07-09 20:57:06 +0530

    Update 0238-product-of-array-except-self.java

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -2,14 +2,16 @@ class Solution {
     public int[] productExceptSelf(int[] nums) {
         int result [] = new int[nums.length];
         result[0] = 1;
+        // result array will store element from the left side of current index's element 
         for (int i = 1; i < nums.length; i++){
             result[i] = result[i - 1] * nums[i - 1];
         }
 
         int rightProduct = 1;
-        for(int i = nums.length-1; i >= 0; i--){
-            result[i] = result[i] * rightProduct;
-            rightProduct *= nums[i];
+        // Here result array will store elements from the right side of nums array 
+        for(int r = nums.length-1; r >= 0; r--){
+            result[r] = result[r] * rightProduct;
+            rightProduct *= nums[r];
         }
         return result;
     }
