eca5f56486394598eb5b213fbc1e1ebf812167c1
Sagar Sahu <sagarsahuji000@gmail.com>
2026-08-03 21:31:08 +0530

    Time: 0 ms (100%), Space: 44.3 MB (37.89%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -3,13 +3,19 @@ class Solution {
         int start = 0, end = nums.length-1;
 
         while (start < end) {
-            int mid = start + (end - start)/2;
-
-            if(nums[mid] > nums[mid+1]){
-                end = mid;
+            // int mid = start + (end - start )/2;
+            // if(nums[mid] > nums[mid+1]){
+            //     end = mid;
+            // }
+            // else {
+            //     start = mid + 1;
+            // }
+            int mid = start + (end - start +1)/2;   // for taking mid as mid + 1;
+            if(nums[mid] > nums[mid - 1]){
+                start = mid ;
             }
             else {
-                start = mid + 1;
+                end = mid - 1 ;
             }
         }
         return start;
