87ee522108e0099a8399feb0034d464e3a0b813f
Sagar Sahu <sagarsahuji000@gmail.com>
2026-08-16 14:17:40 +0530

    Time: 2 ms (91.98%), Space: 48 MB (20.64%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,16 +1,28 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int length=nums.length;
-        int left=0; // it will point zeros
+        int left = 0, right = 0;
 
-        for(int right=0; right<length; right++){    
-            if(nums[right]!= 0){       // right will point non-zero element
-                int temp=nums[right];
-                nums[right]= nums[left];
-                nums[left] = temp;
-                left++;
+        while (right < nums.length) {
+            if(nums[right] != 0){
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] =temp;
+                left ++;
             }
+            right ++;
         }
+
+        // int length=nums.length;
+        // int left=0; // it will point zeros
+
+        // for(int right=0; right<length; right++){    
+        //     if(nums[right]!= 0){       // right will point non-zero element
+        //         int temp=nums[right];
+        //         nums[right]= nums[left];
+        //         nums[left] = temp;
+        //         left++;
+        //     }
+        // }
         
       
     }
