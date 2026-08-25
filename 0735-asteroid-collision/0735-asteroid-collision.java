7d832c4ecf6c1605fabdb2b96116ab25ea8ac314
Sagar Sahu <sagarsahuji000@gmail.com>
2026-08-25 16:53:14 +0530

    Time: 5 ms (67.38%), Space: 47 MB (42.69%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,7 +1,8 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-    Stack <Integer> stack = new Stack<>();
-
+                        // Left to Right
+    /*         
+        Stack <Integer> stack = new Stack<>();
         for(int a : asteroids){
                 
             while (!stack.isEmpty() && stack.peek() > 0 && a < 0 && stack.peek() < -a) {
@@ -24,6 +25,36 @@ class Solution {
         }
 
         return ans;
-    }
+    */
+                    // Right to Left
+        Stack <Integer> stack = new Stack<>();
 
+        for(int i = asteroids.length-1; i >= 0; i--){
+            int a = asteroids[i];
+            boolean alive = true;       // int alive = 1;
+
+            while(alive && !stack.isEmpty() && stack.peek() < 0 && a > 0){
+                if(Math.abs(stack.peek()) < Math.abs(a) ){
+                    stack.pop();
+                } 
+                else if(Math.abs(stack.peek()) == Math.abs(a) ){
+                    stack.pop();
+                    alive = false;      // alive = 0
+                }    
+                else{
+                    alive = false;    // alive =0
+                }                         
+            }
+            
+            if(alive){
+                stack.push(a);
+            }
+            
+        }
+        int ans[] = new int[stack.size()];
+        for(int j = 0; j <= ans.length-1; j++){
+            ans[j] = stack.pop();
+        }
+        return ans;
+    }
 }
