e31d1853fcb4d31fbbb7c9183cb8a93c6e83dabe
Sagar Sahu <sagarsahuji000@gmail.com>
2026-08-26 23:25:16 +0530

    Time: 23 ms (96.95%), Space: 63.9 MB (5.08%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.java
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -2,18 +2,33 @@
 import java.util.Stack;
 
 class StockSpanner {
-    private Stack<int[]> stack;
+    // private Stack<int[]> stack;
 
-    public StockSpanner() {
-        stack = new Stack<>();
-    }
+    // public StockSpanner() {
+    //     stack = new Stack<>();
+    // }
 
-    public int next(int price) {
-        int span = 1;
-        while (!stack.isEmpty() && stack.peek()[0] <= price) {
-            span += stack.pop()[1]; // Add popped span
-        }
-        stack.push(new int[]{price, span}); // Store price and span
-        return span;
+    // public int next(int price) {
+    //     int span = 1;
+    //     while (!stack.isEmpty() && stack.peek()[0] <= price) {
+    //         span += stack.pop()[1]; // Add popped span
+    //     }
+    //     stack.push(new int[]{price, span}); // Store price and span
+    //     return span;
+    // }
+
+    private static int[] price = new int[20];
+    private static int[] span = new int[20];
+    int top = -1;
+
+    public int next(int pr) {
+      int sp = 1;
+
+      while (top != -1 && price[top] <= pr) {
+        sp += span[top--];
+      }
+      price[++top] = pr;
+      span[top] = sp;
+      return sp;
     }
 }
