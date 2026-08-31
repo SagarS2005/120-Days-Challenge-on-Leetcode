e20ed4c58c8b29fd3b39a3e3c625316d84471e29
Sagar Sahu <sagarsahuji000@gmail.com>
2026-08-31 20:15:37 +0530

    Time: 24 ms (45.29%), Space: 47.8 MB (79.51%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,46 +1,42 @@
 class Solution {
     public int calculate(String s) {
-        List<Integer> nums = new ArrayList<>();
-        List<Character> operators = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
         int num = 0;
+        char op = '+';
 
-        for(char ch : s.toCharArray()){
-            if(ch == ' ') continue;
-
+        for(int i = 0; i <= s.length(); i++){
+            char ch = (i == s.length()) ? '+' : s.charAt(i);
             if(Character.isDigit(ch)){
                 num = num*10 + (ch - '0');
             }
-            else{
-                nums.add(num);
-                operators.add(ch);
+
+            else if(ch != ' '){
+                if(op == '+'){
+                    stack.push(num);
+                }
+
+                else if( op == '-'){
+                    stack.push(-num);
+                }
+
+                else if( op == '*'){
+                    stack.push(stack.pop() * num);
+                }
+                else if(op == '/'){
+                    stack.push(stack.pop()/num);
+                }
                 num = 0;
+                op = ch; 
             }
-        }
-        nums.add(num);
-
-        for(int i = 0; i < operators.size(); ){
-            if(operators.get(i) == '*' || operators.get(i) == '/'){
-                int a = nums.get(i);
-                int b = nums.get(i+1);
-                int val = (operators.get(i) == '*') ? a * b : a / b;
-                // int val = (operators.get(i) == '*') ? nums.get(i) * nums.get(i+1):nums.get(i) / nums.get(i+1);
             
-                nums.set(i, val);
-                nums.remove(i+1);
-                operators.remove(i);
-            }
-            else{
-                i++;
-            }
         }
 
-        int result = nums.get(0);
-        for(int i = 0; i < operators.size(); i++){
-            if(operators.get(i) == '+' ) result += nums.get(i + 1);
-            else{
-                result -= nums.get(i + 1);
-            }
+        int ans = 0;
+        while(!stack.isEmpty()){
+            ans += stack.pop();
         }
-        return result;
+
+        return ans;
+
     }
 }
