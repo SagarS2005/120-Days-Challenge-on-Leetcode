d8d7f2ca25603e80e26dc3abe3c3a409be6ef420
Sagar Sahu <sagarsahuji000@gmail.com>
2026-09-08 23:04:48 +0530

    Time: 0 ms (100%), Space: 42.9 MB (40.71%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -1,23 +1,44 @@
 class Solution {
-    public int minAddToMakeValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        int count = 0;
+    // public int minAddToMakeValid(String s) {
+    //     Stack<Character> stack = new Stack<>();
+    //     int count = 0;
 
+    //     for(char ch : s.toCharArray()){
+    //         if(ch == '(' ){
+    //             stack.push(ch);
+    //             count ++;
+    //         }
+    //         else{
+    //             if(!stack.isEmpty() && count >0){
+    //                 stack.pop();
+    //                 count--;
+    //             }
+    //             else{
+    //                 count ++;
+    //             }
+    //         }
+    //     }
+    //     return count;
+    // }
+
+
+    public int minAddToMakeValid(String s) {
+        int countOpen = 0;
+        int countClose = 0;
         for(char ch : s.toCharArray()){
-            if(ch == '(' ){
-                stack.push(ch);
-                count ++;
+            if(ch == '('){
+                countOpen ++;
             }
             else{
-                if(!stack.isEmpty() && count >0){
-                    stack.pop();
-                    count--;
+                if(countOpen > 0){
+                    countOpen --;
                 }
                 else{
-                    count ++;
+                    countClose ++;
                 }
             }
         }
-        return count;
+
+        return countOpen + countClose;
     }
 }
