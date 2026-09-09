8ab3d068ce4f04a5f185d8b33c7de2dbbabe4b10
Sagar Sahu <sagarsahuji000@gmail.com>
2026-09-09 22:39:57 +0530

    Time: 10 ms (96.76%), Space: 47.2 MB (37.11%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,23 +1,40 @@
 class Solution {
+    // public String removeDuplicates(String s) {
+    //     Stack <Character> stack = new Stack<>();
+
+    //     for(int i = 0; i< s.length() ; i++){
+    //         char ch = s.charAt(i);
+
+    //         if(!stack.isEmpty() && stack.peek() == ch){
+    //             stack.pop();
+    //         }
+    //         else{
+    //             stack.push(ch);
+    //         }
+
+    //     }
+    //     StringBuilder res = new StringBuilder();
+    //     while(!stack.isEmpty()){
+    //         res.insert(0, stack.pop());
+    //     }
+
+    //     return res.toString();
+    // }
     public String removeDuplicates(String s) {
-        Stack <Character> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
 
-        for(int i = 0; i< s.length() ; i++){
-            char ch = s.charAt(i);
+        for(char ch : s.toCharArray()){
 
-            if(!stack.isEmpty() && stack.peek() == ch){
-                stack.pop();
+            int len =sb.length();   // last index
+            
+            if(len > 0 && sb.charAt(len-1) == ch){
+                sb.deleteCharAt(len-1);     // delete from the last of sb
             }
             else{
-                stack.push(ch);
+                sb.append(ch);
             }
-
-        }
-        StringBuilder res = new StringBuilder();
-        while(!stack.isEmpty()){
-            res.insert(0, stack.pop());
         }
 
-        return res.toString();
+        return sb.toString();
     }
 }
