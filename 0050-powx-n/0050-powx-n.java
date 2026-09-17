d0d15c37bd82c8e62ca6dbf7a3673d5f41321a7b
Sagar Sahu <sagarsahuji000@gmail.com>
2026-09-17 20:10:54 +0530

    Time: 0 ms (100%), Space: 48.4 MB (10.73%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,24 +1,23 @@
 class Solution {
-    public double myPowHelper(double x, int n) {
-        if(n == 1) return x;
-        if(n == 0) return 1;
+    public double myPow(double x, int n) {
+        long exp = n;
 
-        if(n < 0) {
-            if(n == Integer.MIN_VALUE) {
-                n = -1 * n;
-                return 1 / (x * myPowHelper(x, Integer.MAX_VALUE));
-            }
-
-            n = -1 * n;
-            return 1 / myPowHelper(x, n);
+        if(exp < 0){
+            x = 1/x;
+            exp = -exp;
         }
 
-        double temp = myPowHelper(x, n/2);
+        double ans = 1;
+        while(exp > 0){
+            if(exp % 2 != 0){
+                ans *= x;
+            }
 
-        if(n % 2 == 0) return temp * temp;
-        return x * temp * temp;
-    }
-    public double myPow(double x, int n) {
-        return myPowHelper(x, n);
+            x*=x;
+            exp  /=2;
+        
+        }
+
+        return ans;
     }
 }
