13bf2b116e0a8200e84e9f34dfe1c5dc7d1a5c24
Sagar Sahu <sagarsahuji000@gmail.com>
2026-09-18 23:09:32 +0530

    Time: 0 ms (100%), Space: 42.2 MB (17.47%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int climbStairs(int n) {
+
+        // Recursion
+        // if(n <= 2)
+        // return n;
+
+        // return climbStairs(n-1) + climbStairs(n-2);
+
+
+        // Dynamic Programming
+
+        if(n <=2) return n;
+        int prev = 1;
+        int prev1 = 2;
+
+        for(int i = 3 ; i <= n; i++){
+            int temp = prev + prev1;
+            prev = prev1;
+            prev1 = temp;
+        }
+        return prev1;
+    }
+}
