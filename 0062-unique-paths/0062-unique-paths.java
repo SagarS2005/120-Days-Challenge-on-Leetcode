6b1c61d46f4f5de4e54e9df5cb1bd845a3b406be
Sagar Sahu <sagarsahuji000@gmail.com>
2026-09-20 23:25:23 +0530

    Time: 0 ms (100%), Space: 42.2 MB (47.45%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,17 +1,42 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
-        int[] aboveRow = new int[n];
-        Arrays.fill(aboveRow, 1);
+    // public int uniquePaths(int m, int n) {
+    //     return countPath(m-1, n-1);       
+    // }
 
-        for (int row = 1; row < m; row++) {
-            int[] currentRow = new int[n];
-            Arrays.fill(currentRow, 1);
-            for (int col = 1; col < n; col++) {
-                currentRow[col] = currentRow[col - 1] + aboveRow[col];
-            }
-            aboveRow = currentRow;
+    // private int countPath(int row, int col){
+    //     if (row == 0 || col == 0){
+    //         return 1;
+    //     }
+    //     return countPath(row-1, col) + countPath(row, col-1);
+    // }
+    // public int uniquePaths(int m, int n) {
+    //     int[] aboveRow = new int[n];
+    //     Arrays.fill(aboveRow, 1);
+
+    //     for (int row = 1; row < m; row++) {
+    //         int[] currentRow = new int[n];
+    //         Arrays.fill(currentRow, 1);
+    //         for (int col = 1; col < n; col++) {
+    //             currentRow[col] = currentRow[col - 1] + aboveRow[col];
+    //         }
+    //         aboveRow = currentRow;
+    //     }
+
+    //     return aboveRow[n - 1];        
+    // }
+    public int uniquePaths(int m, int n) {
+        int dp[][] = new int[m][n];
+        return countPath(m-1 , n-1 , dp);
+    }
+    private int countPath(int row, int col, int [][] dp){
+        if (row == 0 || col == 0){
+            return 1;
         }
 
-        return aboveRow[n - 1];        
+        if(dp[row][col] != 0){
+            return dp[row][col];
+        }
+        dp[row][col] = countPath(row-1, col,dp) + countPath(row, col-1,dp);
+        return dp[row][col];
     }
 }
